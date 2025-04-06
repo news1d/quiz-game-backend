d7a1c7c0f6d7ba845df90ddc858572d9869bde95
news1d <news1dee@yandex.ru>
2025-04-06 16:49:11 +0300

    Added default value for sortBy in GetQuestionsQueryParams

--- src/features/quiz-game/questions/api/input-dto/get-questions-query-params.input-dto.ts
+++ src/features/quiz-game/questions/api/input-dto/get-questions-query-params.input-dto.ts
@@ -2,7 +2,7 @@ import { QuestionsSortBy } from './questions-sort-by';
 import { BaseSortablePaginationParams } from '../../../../../core/dto/base.query-params.input-dto';
 
 export class GetQuestionsQueryParams extends BaseSortablePaginationParams<QuestionsSortBy> {
-  sortBy: QuestionsSortBy.CreatedAt;
+  sortBy: QuestionsSortBy = QuestionsSortBy.CreatedAt;
   bodySearchTerm: string | null = null;
   publishedStatus: string = 'all';
 }
